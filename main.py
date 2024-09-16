5099204ac9b9e31f9c2608a66b7179d0b0ff725f
starry-skies <mmazin2028@kehillahstudent.org>
2024-09-16 09:16:21 -0700

    finished a02 exercise

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 import turtle
+numberofsquare = 5
+square = 40
+angle = 90
+def draw():
+     for i in range (2):
+        turtle.forward(square * (numberofsquare - 4))
+        turtle.left(angle)
+     for i in range (2):
+       turtle.forward(square * (numberofsquare - 3))
+       turtle.left(angle)
+     for i in range (2):
+         turtle.forward(square * (numberofsquare - 2))
+         turtle.left(angle)
+     for i in range (2):
+         turtle.forward(square * (numberofsquare - 1))
+         turtle.left(angle)
+     for i in range (3):
+        turtle.forward(square * numberofsquare)
+        turtle.left(angle)
+    
+    
+
+draw()
+turtle.exitonclick()
